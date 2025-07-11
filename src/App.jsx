8b726bddd360cdb06f851bb4b8b2416c64beb0fb
Vincent-Caraga <vincentccaraga@gmail.com>
2025-07-11 16:00:32 +0800

    new commit
    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,22 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import Header from './components/Header/Header'
-import AboutUs from './components/AboutUs/AboutUs'
-import Service from './components/Service/Service'
-import Branches from './components/Branches/Branches'
-import Footer from './components/Footer/Footer'
-
-
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header/Header";
+import AboutUs from "./components/AboutUs/AboutUs";
+import Service from "./components/Service/Service";
+import Branches from "./components/Branches/Branches";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
     <div>
-     <Navbar/>
-      <Header/>
-      <AboutUs/>
-      <Service/>
-      <Branches/>
-      <Footer/>
+      <Navbar />
+      <Header />
+      <AboutUs />
+      <Service />
+      <Branches />
+      <Footer />
     </div>
-     
-    
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
---
+++ src/pages/BookNow/BookNow.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BookNow = () => {
+  return (
+    <div>
+      <p>no content yet!</p>
+    </div>
+  );
+};
+
+export default BookNow;
